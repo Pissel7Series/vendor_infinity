77917440e1e46065a169caf6bd93979ef853844f
Austin Young <austineyoung2000@gmail.com>
2025-06-16 17:25:53 -0400

    config: now shipping GoogleFaceUnlock and AOSPA Impl
    * pixel 7-9 devices must now include flag " TARGET_SUPPORTS_GFU := true " for GoogleFaceUnlock
    * otherwise AOSPA Impl is shipped by defualt

--- config/common.mk
+++ config/common.mk
@@ -247,6 +247,9 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
 
 # FaceUnlock
 ifneq ($(TARGET_FACE_UNLOCK_SUPPORTED),false)
+ifeq ($(TARGET_SUPPORTS_GFU),true)
+$(call inherit-product-if-exists, vendor/google/faceunlock/config.mk)
+else
 PRODUCT_PACKAGES += \
     FaceUnlock
 
@@ -255,6 +258,7 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.hardware.biometrics.face.xml
+endif
 endif
 
 # Filesystems tools
