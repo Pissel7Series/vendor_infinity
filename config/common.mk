4d2b8490ea1cb216671a341a14609a168dee33e1
tejas101k <tejassingh649@rediffmail.com>
2026-03-02 01:42:10 +0000

    config: Build SB overlay

--- config/common.mk
+++ config/common.mk
@@ -359,7 +359,8 @@ PRODUCT_PACKAGES += \
     DocumentsUIOverlay \
     NetworkStackOverlay \
     NfcOverlay \
-    PermissionControllerOverlay
+    PermissionControllerOverlay \
+    SBHeightOverlay
 
 # Translations
 CUSTOM_LOCALES += \
